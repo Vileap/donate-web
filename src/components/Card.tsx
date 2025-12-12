f2d41813e654f0e02745629c16064bfc5cefd8c0
vileap <vileapvong@gmail.com>
2025-12-12 19:53:45 +0700

    fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,42 +17,49 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
   };
 
   return (
-    <li className="my-6">
-      <a
-        href={href}
-        className="inline-block text-lg font-medium text-[#FAB45C] decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
-      >
-        {secHeading ? (
-          <h2 {...headerProps}>{title}</h2>
-        ) : (
-          <h3 {...headerProps}>{title}</h3>
-        )}
-      </a>
-      <div className="md:text-lg text-base font-bold capitalize italic text-[#ecc689]">
-        {location ? (
-          <span className="flex flex-row items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 28"
-              fill="#000000"
-              stroke="#ffffff"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-map-pin-icon lucide-map-pin"
-            >
-              <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
-              <circle cx="12" cy="10" r="3" />
-            </svg>
-            <p>{location}</p>
-          </span>
-        ) : (
-          ""
-        )}
+    <li className="my-6 flex items-center justify-between rounded-md border p-4 shadow-md">
+      <div>
+        <a
+          href={href}
+          className="inline-block text-lg font-bold text-[#ecc689] decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        >
+          {secHeading ? (
+            <h2 {...headerProps}>{title}</h2>
+          ) : (
+            <h3 {...headerProps}>{title}</h3>
+          )}
+        </a>
+        <div className="md:text-lg text-base font-bold capitalize italic text-[#ecc689]">
+          {location ? (
+            <span className="flex flex-row items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 28"
+                fill="#000000"
+                stroke="#ffffff"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-map-pin-icon lucide-map-pin"
+              >
+                <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
+                <circle cx="12" cy="10" r="3" />
+              </svg>
+              <p className="text-blue-500">{location}</p>
+            </span>
+          ) : (
+            ""
+          )}
+        </div>
+        <p>{description}</p>
       </div>
-      <p>{description}</p>
+      <div>
+        <a {...headerProps} href={href}>
+          View
+        </a>
+      </div>
     </li>
   );
 }
